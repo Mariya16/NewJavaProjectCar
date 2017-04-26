0edd59af46b68485cea0c00d74477e09f29f7b17
Mariya16 <mariya.yar9@gmail.com>
2017-04-26 17:41:04 +0300

    Update code formating

--- src/Test_ArrayList.java
+++ src/Test_ArrayList.java
@@ -1,29 +1,28 @@
-
-
-		class Test_ArrayList {
-			int a;
-			int b;
-
-			 public void setData(int a ,int b){
-			  a = a;
-			  b = b;
-			 }
-			 public void showData(){
-			   System.out.println("Value of A ="+a);
-			   System.out.println("Value of B ="+b);
-			 }
-			 public static void main(String args[]){
-			   Account obj = new Account();
-			   obj.setData(2,3);
-			   obj.showData();
-				public static void main (String agrs[]) {
-				ArrayList<object> a = new ArrayList<object>();
-				a.add(0,1);
-				a.add(1,100);
-				a.add(2,3);
-				a.add(3,4);
-				a.remove((Object)100);
-				System.out.println(a);
-				}
-				}
+class Test_ArrayList {
+	int a;
+	int b;
+	
+	 public void setData(int a ,int b){
+	  a = a;
+	  b = b;
+	 }
+	 public void showData(){
+	   System.out.println("Value of A ="+a);
+	   System.out.println("Value of B ="+b);
+	 }
+	 public static void main(String args[]){
+	   Account obj = new Account();
+	   obj.setData(2,3);
+	   obj.showData();
+		public static void main (String agrs[]) {
+			ArrayList<object> a = new ArrayList<object>();
+			a.add(0,1);
+			a.add(1,100);
+			a.add(2,3);
+			a.add(3,4);
+			a.remove((Object)100);
+			System.out.println(a);
+		}
+	 }
+}
 
